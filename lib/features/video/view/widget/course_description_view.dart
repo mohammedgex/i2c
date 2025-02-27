d739da8d99118260c5f14895b6aa78acdcdbbe2b
Meharab-Islam <mdnibir178@gmail.com>
2025-02-27 14:28:19 +0600

    all error fix

--- lib/features/video/view/widget/course_description_view.dart
+++ lib/features/video/view/widget/course_description_view.dart
@@ -1,8 +1,10 @@
 // ignore_for_file: unnecessary_null_comparison
 
+import 'package:colorful_safe_area/colorful_safe_area.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:skill_grow/core/colors/app_colors.dart';
 import 'package:skill_grow/core/widgets/appbar.dart';
 import 'package:skill_grow/core/widgets/texts.dart';
 import 'package:skill_grow/features/video/controller/learning_data_controller.dart';
@@ -14,21 +16,26 @@ class CourseDescriptionView extends StatelessWidget {
   Widget build(BuildContext context) {
     LearningDataController learningDataController = Get.find<LearningDataController>();
     return Scaffold(
-      body: Padding(padding: 
-      EdgeInsets.symmetric(horizontal: 15.sp), child: Column(
-        children: [
-          MyCustomAppBar(
-            verticalPadding: 0,horizontalPadding: 0, isShowNotification: true,isShowbackButton: true,
+      body: ColorfulSafeArea(
+        color: AppColors.scaffoldBackgroundColor,
+        child: Padding(padding: 
+        EdgeInsets.symmetric(horizontal: 15.sp), child: SingleChildScrollView(
+          child: Column(
+            children: [
+              MyCustomAppBar(
+                verticalPadding: 0,horizontalPadding: 0, isShowNotification: true,isShowbackButton: true,
+              ),
+              Obx((){
+                if (learningDataController.course.value!.data.description.isEmpty || learningDataController.course.value!.data.description == null) {
+                  return GlobalText(text: "No Description Available");
+                } else {
+                  return HtmlGlobalText(text: learningDataController.course.value!.data.description, softWrap: true);
+                }
+              })
+            ],
           ),
-          Obx((){
-            if (learningDataController.course.value!.data.description.isEmpty || learningDataController.course.value!.data.description == null) {
-              return GlobalText(text: "No Description Available");
-            } else {
-              return HtmlGlobalText(text: learningDataController.course.value!.data.description, softWrap: true);
-            }
-          })
-        ],
-      ),),
+        ),),
+      ),
     );
   }
 }
